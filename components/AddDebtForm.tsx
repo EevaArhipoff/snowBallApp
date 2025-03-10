d11fbf580421d8ae1f577a39fcd277c8a0831265
EevaArhipoff <eeva.arhipoff@gmail.com>
2025-03-10 17:54:20 +0200

    Formatting addDebtForm

--- components/AddDebtForm.tsx
+++ components/AddDebtForm.tsx
@@ -1,172 +1,179 @@
 import React, { useState } from 'react';
 import { useThemeColor } from '@/hooks/useThemeColor';
-import { View, Text, TextInput, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, SafeAreaView, Button } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import CustomButton from '@/components/CustomButton';
 import { FONTS } from '@/constants/constants';
 
 
 interface formProps {
-    name?: string;
-    amount?: string;
-    monthlyPayment?: string,
+    name: string;
+    amount: string;
+    monthlyPayment: string,
 }
 
-const AddDebtForm: React.FC<formProps> = ({
-    name,
-    amount,
-    monthlyPayment,
-}) => {
+const AddDebtForm: React.FC = () => {
 
-    const { control, handleSubmit, formState: { errors } } = useForm<formProps>({})
+    const { control, handleSubmit, reset, formState: { errors } } = useForm<formProps>()
     const [submittedData, setSubmittedData] = useState<formProps | null>(null)
     const textColor = useThemeColor({}, 'text')
-    const buttonBorder = useThemeColor({}, 'buttonBorder')
+    const inputBorder = useThemeColor({}, 'button')
 
     const onSubmit = (data: formProps) => {
-        // Simulate form submission
-        console.log('Submitted Data:', data)
-        //   setSubmittedData(data);
+        setSubmittedData(data);
+        reset();
     };
 
 
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <Text style={[styles.helperText, { color: textColor }]}>Anna lisättävän velan tiedot. Anna velalle nimi, josta voit itse helposti tunnistaa mikä velka on kyseessä.</Text>
-                <Controller
-                    control={control}
-                    name="name"
-                    rules={{ required: 'Anna velalle nimi' }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            style={[styles.input, { color: textColor }, { borderColor: buttonBorder }]}
-                            placeholder="Velan nimi"
-                            placeholderTextColor={'#CDCDCD'}
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                        />
-                    )}
-                />
-                {errors.name && <Text style={styles.errorText}>{errors.name.message}</Text>}
+                <View style={styles.inputContainer}>
+                    <Text style={[styles.helperText, { color: textColor }]}>Anna lisättävän velan tiedot. Anna velalle nimi, josta voit itse helposti tunnistaa mikä velka on kyseessä.</Text>
+                    <Controller
+                        control={control}
+                        name="name"
+                        rules={{ required: 'Anna velalle nimi' }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={[styles.input, { color: textColor }, { borderColor: inputBorder }]}
+                                placeholder="Velan nimi"
+                                placeholderTextColor={'#CDCDCD'}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                        )}
+                    />
+                    {errors.name && <Text style={styles.errorText}>{errors.name.message}</Text>}
+                </View>
 
-                <Text style={[styles.helperText, { color: textColor }]}>Syötä jäljellä olevan velan kokonaismäärä:</Text>
-                <Controller
-                    control={control}
-                    name="amount"
-                    rules={{ required: 'Anna loppuvelan määrä.' }}
-                    render={({ field: { onChange, value } }) => (
-                        <TextInput
-                            style={[styles.input, { color: textColor }, { borderColor: buttonBorder }]}
-                            placeholder="Jäljellä olevan velan määrä"
-                            placeholderTextColor={'#CDCDCD'}
-                            value={value}
-                            onChangeText={text => {
-                                // Only numbers and "," are allowed
-                                let formattedText = text.replace(/[^0-9,]/g, '');
+                <View style={styles.inputContainer}>
+                    <Text style={[styles.helperText, { color: textColor }]}>Syötä jäljellä olevan velan kokonaismäärä:</Text>
+                    <Controller
+                        control={control}
+                        name="amount"
+                        rules={{ required: 'Anna loppuvelan määrä.' }}
+                        render={({ field: { onChange, value } }) => (
+                            <TextInput
+                                style={[styles.input, { color: textColor }, { borderColor: inputBorder }]}
+                                placeholder="Esim. 1024,20"
+                                placeholderTextColor={'#CDCDCD'}
+                                value={value}
+                                onChangeText={text => {
+                                    // Only numbers and "," are allowed
+                                    let formattedText = text.replace(/[^0-9,]/g, '');
 
-                                // Accepting only one comma
-                                const parts = formattedText.split(',');
-                                if (parts.length > 2) {
-                                    return;
-                                }
+                                    // Accepting only one comma
+                                    const parts = formattedText.split(',');
+                                    if (parts.length > 2) {
+                                        return;
+                                    }
 
-                                // Can't start with comma
-                                if (formattedText.startsWith(',')) {
-                                    formattedText = '';
-                                }
+                                    // Can't start with comma
+                                    if (formattedText.startsWith(',')) {
+                                        formattedText = '';
+                                    }
 
-                                onChange(formattedText);
-                            }}
-                            keyboardType="decimal-pad"
-                        />
-                    )}
-                />
-                {errors.amount && <Text style={styles.errorText}>{errors.amount.message}</Text>}
+                                    onChange(formattedText);
+                                }}
+                                keyboardType="decimal-pad"
+                            />
+                        )}
+                    />
+                    {errors.amount && <Text style={styles.errorText}>{errors.amount.message}</Text>}
+                </View>
 
-                <Text style={[styles.helperText, { color: textColor }]}>Ilmoita velan kuukausilyhennyksen määrä. Jos lainaa ei tällä hetkellä lyhennetä, aseta määräksi 0:</Text>
-                <Controller
-                    control={control}
-                    name="monthlyPayment"
-                    rules={{ required: 'Ilmoita velan kuukausilyhennyksen määrä.' }}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            style={[styles.input, { color: textColor }, { borderColor: buttonBorder }]}
-                            placeholder="Kuukausilyhennys tällä hetkellä"
-                            placeholderTextColor={'#CDCDCD'}
-                            onBlur={onBlur}
-                            value={value}
-                            onChangeText={text => {
-                                // Only numbers and "," are allowed
-                                let formattedText = text.replace(/[^0-9,]/g, '');
+                <View style={styles.inputContainer}>
+                    <Text style={[styles.helperText, { color: textColor }]}>Ilmoita velan tämänhetkisen kuukausilyhennyksen määrä. Jos lainaa ei tällä hetkellä lyhennetä, aseta määräksi 0:</Text>
+                    <Controller
+                        control={control}
+                        name="monthlyPayment"
+                        rules={{ required: 'Ilmoita velan kuukausilyhennyksen määrä.' }}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={[styles.input, { color: textColor }, { borderColor: inputBorder }]}
+                                placeholder="Esim. 110,50"
+                                placeholderTextColor={'#CDCDCD'}
+                                onBlur={onBlur}
+                                value={value}
+                                onChangeText={text => {
+                                    // Only numbers and "," are allowed
+                                    let formattedText = text.replace(/[^0-9,]/g, '');
 
-                                // Accepting only one comma
-                                const parts = formattedText.split(',');
-                                if (parts.length > 2) {
-                                    return;
-                                }
+                                    // Accepting only one comma
+                                    const parts = formattedText.split(',');
+                                    if (parts.length > 2) {
+                                        return;
+                                    }
 
-                                // Can't start with comma
-                                if (formattedText.startsWith(',')) {
-                                    formattedText = '';
-                                }
+                                    // Can't start with comma
+                                    if (formattedText.startsWith(',')) {
+                                        formattedText = '';
+                                    }
 
-                                onChange(formattedText);
-                            }}
-                            keyboardType="decimal-pad"
-                        />
-                    )}
-                />
-                {errors.monthlyPayment && <Text style={styles.errorText}>{errors.monthlyPayment.message}</Text>}
+                                    onChange(formattedText);
+                                }}
+                                keyboardType="decimal-pad"
+                            />
+                        )}
+                    />
+                    {errors.monthlyPayment && <Text style={styles.errorText}>{errors.monthlyPayment.message}</Text>}
+                </View>
 
-                <CustomButton title="Tallenna" onPress={handleSubmit(onSubmit)}></CustomButton>
+                <View style={styles.buttonView}>
+                    <CustomButton title="Tallenna" onPress={handleSubmit(onSubmit)}></CustomButton>
+                </View>
 
+                {/* Show submitted data to user */}
                 {submittedData && (
                     <View style={styles.submittedContainer}>
-                        <Text style={styles.submittedTitle}>Annetut tiedot::</Text>
+                        <Text style={styles.submittedTitle}>Lisätty uusi velka:</Text>
                         <Text>Nimi: {submittedData.name}</Text>
                         <Text>Velan kokonaismäärä: {submittedData.amount}</Text>
                         <Text>Velan kuukausilyhennys: {submittedData.monthlyPayment}</Text>
-
                     </View>
                 )}
+
             </View>
         </SafeAreaView>
     )
 }
 
-
-
 const styles = StyleSheet.create({
     container: {
-        padding: 16,
+        padding: 18,
+        borderWidth: 1.5,
+        borderColor: '#B8860B',
+    },
+    inputContainer: {
+        marginBottom: 15,
+    },
+    buttonView: {
+        alignSelf: 'center',
     },
     input: {
         height: 40,
         borderWidth: 1,
         marginBottom: 5,
-        marginTop: 5,
         padding: 8,
     },
     errorText: {
         color: 'red',
-        fontSize: 15,
-        marginBottom: 15,
-
+        fontSize: 13,
     },
     submittedContainer: {
         marginTop: 20,
         padding: 10,
         backgroundColor: '#f9f9f9',
-        borderRadius: 5,
+        borderRadius: 8,
     },
     submittedTitle: {
         fontWeight: 'bold',
         marginBottom: 5,
     },
     helperText: {
-        marginTop: 15,
+        marginBottom: 5,
         fontFamily: FONTS.body,
         lineHeight: 20,
         fontSize: 16,

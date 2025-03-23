bc59dccefbdeba6cf8722895318b72eb8ac0faa0
EevaArhipoff <eeva.arhipoff@gmail.com>
2025-03-23 16:24:17 +0200

    Added SQL features and cleaned test rows

--- app/(tabs)/debts.tsx
+++ app/(tabs)/debts.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Collapsible } from '@/components/Collapsible';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -6,8 +7,36 @@ import { ThemedView } from '@/components/ThemedView';
 import { Colors } from '@/constants/Colors';
 import HeaderView from '@/components/HeaderView';
 import AddDebtForm from '@/components/AddDebtForm';
+import ListOfDebts from '@/components/ListOfDebts';
+import { useDatabase } from '@/SQL/dataBaseFunctions';
 
 export default function TabTwoScreen() {
+
+  const { db, fetchAllUnpaidDebts, isDbReady } = useDatabase()
+  const [debts, setDebts] = useState<any[]>([])
+
+  useEffect(() => {
+    if (!isDbReady) return
+
+    const fetchData = async () => {
+      const data = await fetchAllUnpaidDebts()
+      setDebts(data)
+    };
+
+    fetchData();
+  }, [isDbReady])
+
+  // Fetching message if database is loading
+  if (!isDbReady) {
+    return <ThemedText>Ladataan tietokantaa...</ThemedText>
+  }
+
+  const handleNewDebt = async () => {
+    const debts = await fetchAllUnpaidDebts();
+    setDebts(debts);
+
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: Colors.light.headerBackGround, dark: Colors.dark.headerBackGround }}
@@ -19,13 +48,11 @@ export default function TabTwoScreen() {
       </ThemedView>
       <ThemedText>Tältä sivulta löydät listaamasi velat. Voit myös lisätä, muokata tai poistaa velkoja.</ThemedText>
       <Collapsible title="Velat">
-        <ThemedText>
-          <ThemedText type="defaultSemiBold">Velka nro 1</ThemedText>
-          <ThemedText type="defaultSemiBold">Velka nro 2</ThemedText>
-        </ThemedText>
+        <ThemedText>{debts.length > 0 ? `Velkoja: ${debts.length} kpl` : 'Ei velkoja'}</ThemedText>
+        <ListOfDebts debts={debts} />
       </Collapsible>
       <Collapsible title="Lisää velka">
-        <AddDebtForm />
+        <AddDebtForm onDebtAdd={handleNewDebt} />
       </Collapsible>
     </ParallaxScrollView>
   );
